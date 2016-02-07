7bd6453f0b13f5f1abe42ef5abf00f9d0c97da8f
Matt Bierner <mattbierner@gmail.com>
2016-02-06 17:33:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# The Whole Rainbow
+Script that uploads an image of every RGB color to Instagram. Configured to upload a new color every five minutes, slowly working its way through all 16,777,216 24bit RGB colors. It should finish sometime in the 2170s. We will never see the whole rainbow.
+
+[See it in action over at @thewholerainbow](wholerainbow), or, for an even more colorful social media experience, [check out Blot're](https://blot.re).
+
+### Why don't the colors in the uploaded images exactly match the targeted hex values?
+Instagram takes jpeg images and performs some post processing that may alter the precise color values.
+
+### Will Instagram have a problem with this?
+I hope not. Their public API lacks the ability to upload images (WTF), so this script makes use of an unofficial API to automate the upload process. The script is designed to run very slowly, and the uploaded images are extremely tiny, so it does not consume many resources at all.
+
+This is a fun little project that shouldn't bother anyone and improves the community. Just in case however, I recommend using a burner account if you plan on using the scripts found in this repo. If @thewholerainbow suddenly stops working, most likely someone at Instagram just hates rainbows.
+
+# Running
+If you notice that the @thewholerainbow has stopped uploading, feel free to use these scripts to continue its mission. The uploads are also tagged with `#thewholerainbow` plus `#HEX_COLOR` so you can track what has been uploaded so far instead of starting from scratch.
+
+The main logic is `main.py`. When run, the script uploads a single color and then updates some persisted data and exits.
+
+First, make sure you have all the require dependencies:
+
+```bash
+$ pip install requests
+```
+
+And make sure to set the following environment variables to give the script permission to upload to an Instagram account:
+
+```bash
+$ export INSTAGRAM_USER_ID="your user name"
+$ export INSTAGRAM_USER_PASSWORD="your password"
+```
+
+Then just run the script, `$ python main.py`. The current (to be uploaded) color is saved to `color.data` as an hex color.
+
+Run the script using cron to upload all the rainbow. Here's the config to run once every five minutes:
+
+```
+*/5 * * * * /usr/bin/python thewholerainbow/main.py
+```
+
+# Credits
+Python Instagram uploader copied from https://github.com/lukecyca/python-instagram-upload
+
+
+[wholerainbow]: https://www.instagram.com/thewholerainbow/
---
+++ main.py
@@ -0,0 +1,96 @@
+import os
+import base64
+from PIL import Image
+import urllib
+import instagram
+from cookielib import LWPCookieJar
+import requests
+import time
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+IMAGE_FILE = os.path.join(ROOT, 'color.jpg')
+DATA_FILE = os.path.join(ROOT, 'color.data')
+
+INITIAL_COLOR = 0x0
+MAX_COLOR = 0xffffff
+
+# Disable actual uploads
+DEBUG = False
+
+username = os.environ.get('INSTAGRAM_USER_ID')
+password = os.environ.get('INSTAGRAM_USER_PASSWORD')
+
+def initial_sesion(force_update=False):
+    """Get Instagram session"""
+    s = requests.Session()
+    s.cookies = LWPCookieJar('cookiejar')
+    if force_update or not os.path.isfile('cookiejar'):
+        session = instagram.InstagramSession(session=s)
+        if not session.login(username, password):
+            return None
+        s.cookies.save()
+        return session
+    else:
+        s.cookies.load(ignore_discard=True)
+    return instagram.InstagramSession(session=s)
+
+
+def int_rgb_tuple(num):
+    return (num & 0xff, (num >> 8) & 0xff, (num >> 16) & 0xff)
+
+def generate_image(color):
+    """Create a solid image of color"""
+    color_tuple = int_rgb_tuple(color)
+    return Image.new('RGB', (500, 500), color=color_tuple)
+
+def generate_image_file(color):
+    """Create a file for a solid image of color"""
+    img = generate_image(color)
+    img.save(IMAGE_FILE, quality=100)
+    return IMAGE_FILE
+
+def get_initial_color():
+    """Get color to start with on boot"""
+    if os.path.isfile(DATA_FILE):
+        with open(DATA_FILE, 'r') as f:
+            data = f.readline()
+            print data
+            return int(data, base=16)
+    else:
+        return INITIAL_COLOR
+
+def write_color_data(color):
+    """Update persisted current color data"""
+    with open(DATA_FILE, 'w') as f:
+        f.write("{0:06x}".format(color))
+
+def post_the_rainbow(start_color, session):
+    color = start_color
+    if color > MAX_COLOR:
+        print("Out of rainbow")
+        return
+
+    caption = "#{0:06x} #thewholerainbow".format(color)
+    image_file = generate_image_file(color)
+
+    if not DEBUG:
+        media_id = session.upload_photo(image_file)
+        if not media_id:
+            print("Error uploading", color)
+            return
+        if not session.configure_photo(media_id, caption):
+            print("Error posting", color)
+
+    color = color + 1
+    write_color_data(color)
+
+def main():
+    intial_color = get_initial_color()
+    session = initial_sesion()
+    if initial_sesion is None:
+        print("Error loging in to account")
+        return
+    post_the_rainbow(intial_color, session)
+
+if __name__ == "__main__":
+    main()
